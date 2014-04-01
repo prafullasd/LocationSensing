d5b357143c9843e0f29ffdb77f9f64c45b86d9e1
Prafulla <prafulla@mit.edu>
2014-04-01 17:45:22 -0400

    something

--- gen/com/example/locationsensing/R.java
+++ gen/com/example/locationsensing/R.java
@@ -1286,13 +1286,17 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f060019;
         public static final int action_menu_presenter=0x7f06001a;
         public static final int action_mode_close_button=0x7f060027;
-        public static final int action_settings=0x7f060043;
+        public static final int action_settings=0x7f060047;
         public static final int activity_chooser_view_content=0x7f060028;
         public static final int always=0x7f06000f;
         public static final int beginning=0x7f060014;
-        public static final int button1=0x7f060040;
-        public static final int button2=0x7f060041;
-        public static final int button3=0x7f060042;
+        public static final int button1=0x7f060044;
+        public static final int button11=0x7f060040;
+        public static final int button12=0x7f060041;
+        public static final int button2=0x7f060045;
+        public static final int button3=0x7f060046;
+        public static final int button4=0x7f060042;
+        public static final int button5=0x7f060043;
         public static final int checkbox=0x7f060030;
         public static final int collapseActionView=0x7f060011;
         public static final int default_activity_button=0x7f06002b;
@@ -1471,6 +1475,8 @@ containing a value of this type.
         public static final int button1Text=0x7f07002b;
         public static final int button2Text=0x7f07002c;
         public static final int button3Text=0x7f07002d;
+        public static final int button4Text=0x7f07002f;
+        public static final int button5Text=0x7f070030;
         /**  Button in confirmation dialog to enable Google Play services.  Clicking it
         will direct user to application settings of Google Play services where they
         can enable it [CHAR LIMIT=40] 
--- src/com/example/locationsensing/MainActivity.java
+++ src/com/example/locationsensing/MainActivity.java
@@ -8,13 +8,25 @@ import java.io.IOException;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesClient;
 import com.google.android.gms.location.LocationClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.DownloadManager;
+import android.app.DownloadManager.Query;
+import android.app.DownloadManager.Request;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
 import android.content.IntentSender;
+import android.database.Cursor;
 import android.location.Location;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 
@@ -24,30 +36,58 @@ import android.widget.Toast;
  *
  */
 public class MainActivity extends Activity implements GooglePlayServicesClient.ConnectionCallbacks,
-GooglePlayServicesClient.OnConnectionFailedListener {
+GooglePlayServicesClient.OnConnectionFailedListener, LocationListener {
 	private LocationClient mLocationClient;
     private Location mCurrentLocation;
     private File file;
     private FileWriter filewriter;
-	@Override
+
+    // Milliseconds per second
+    private static final int MILLISECONDS_PER_SECOND = 1000;
+    // Update frequency in seconds
+    public static final int UPDATE_INTERVAL_IN_SECONDS = 5;
+    // Update frequency in milliseconds
+    private static final long UPDATE_INTERVAL =
+            MILLISECONDS_PER_SECOND * UPDATE_INTERVAL_IN_SECONDS;
+    // The fastest update frequency, in seconds
+    private static final int FASTEST_INTERVAL_IN_SECONDS = 1;
+    // A fast frequency ceiling in milliseconds
+    private static final long FASTEST_INTERVAL =
+            MILLISECONDS_PER_SECOND * FASTEST_INTERVAL_IN_SECONDS;
+    // Define an object that holds accuracy and frequency parameters
+    private LocationRequest mLocationRequest;
+    int count = 0;
+    final String DOWNLOAD_FILE = "http://web.mit.edu/21w.789/www/papers/griswold2004.pdf ";
+    private long enqueue;
+    private DownloadManager dm;
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mLocationClient = new LocationClient(this, this, this);
-        file = new File(Environment.getExternalStoragePublicDirectory(DOWNLOAD_SERVICE), "logfile1.txt");
+        // Create the LocationRequest object
+        mLocationRequest = LocationRequest.create();
+        // Set the update interval to 5 seconds
+        mLocationRequest.setInterval(UPDATE_INTERVAL);
+        // Set the fastest update interval to 1 second
+        mLocationRequest.setFastestInterval(FASTEST_INTERVAL);
+    }
+    
+    @Override
+    protected void onStart() {
+        super.onStart();
+        count +=1;
+        file = new File(Environment.getExternalStoragePublicDirectory(DOWNLOAD_SERVICE), "logfiles"+count+".txt");
         try {
 			file.createNewFile();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	}
-    
-    @Override
-    protected void onStart() {
-        super.onStart();
         // Connect the client.
+        
         mLocationClient.connect();
+        
         try {
 			filewriter = new FileWriter(file);
 		} catch (IOException e) {
@@ -57,7 +97,6 @@ GooglePlayServicesClient.OnConnectionFailedListener {
     }
     @Override
     protected void onStop() {
-        // Disconnecting the client invalidates it.
         mLocationClient.disconnect();
         try {
         	filewriter.close();
@@ -68,23 +107,66 @@ GooglePlayServicesClient.OnConnectionFailedListener {
         super.onStop();
     }
 
-
-
-    
-    public void senseGps(View v){
-    	
+    @SuppressLint("NewApi")
+	public void onDownloadClick(View v){
+    	dm = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
+        Request request = new Request(
+                Uri.parse(DOWNLOAD_FILE));
+        enqueue = dm.enqueue(request);
     }
     
-    public void senseWifi(View v){
+    @SuppressLint("InlinedApi")
+	public void downloadStatus(View v){
+    	Toast.makeText(this, "Status: "+DownloadManager.COLUMN_STATUS+" Downloaded: "+DownloadManager.COLUMN_BYTES_DOWNLOADED_SO_FAR, Toast.LENGTH_SHORT).show();
+    }
+    @SuppressLint("InlinedApi")
+	public void showDownload(View view) {
+        Intent i = new Intent();
+        i.setAction(DownloadManager.ACTION_VIEW_DOWNLOADS);
+        startActivity(i);
+    }
 
+    public void sensenNetworkProvider(View v){
+    	Toast.makeText(this, "Using Network Provider", Toast.LENGTH_SHORT).show();
+    	mLocationRequest.setPriority(
+                LocationRequest.PRIORITY_NO_POWER);
+    	mLocationClient.requestLocationUpdates(mLocationRequest, this);
+    }
+
+    public void senseLeastAccuracy(View v){
+    	Toast.makeText(this, "Hmm", Toast.LENGTH_SHORT).show();
+    	mLocationRequest.setPriority(
+                LocationRequest.PRIORITY_NO_POWER);
+    	mLocationClient.requestLocationUpdates(mLocationRequest, this);
     }
     
-    public void senseBoth(View v){
+    public void senseLowerAccuracy(View v){
+    	Toast.makeText(this, "Hmm", Toast.LENGTH_SHORT).show();
+    	mLocationRequest.setPriority(
+                LocationRequest.PRIORITY_LOW_POWER);
+    	mLocationClient.requestLocationUpdates(mLocationRequest, this);
+    }
+    
+    public void senseLowAccuracy(View v){
+    	Toast.makeText(this, "Hmm", Toast.LENGTH_SHORT).show();
+    	mLocationRequest.setPriority(
+                LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+    	mLocationClient.requestLocationUpdates(mLocationRequest, this);
+    }
+    
+    public void senseHighAccuracy(View v){
+    	Toast.makeText(this, "Hmm", Toast.LENGTH_SHORT).show();
+    	mLocationRequest.setPriority(
+                LocationRequest.PRIORITY_HIGH_ACCURACY);
+    	mLocationClient.requestLocationUpdates(mLocationRequest, this);
+    }
+    
+    public void senseLastLocation(View v){
     	mCurrentLocation = mLocationClient.getLastLocation();
     	Toast.makeText(this,"Location is:" + mCurrentLocation.getLatitude()+" "+mCurrentLocation.getLongitude()+" and accuracy is "+mCurrentLocation.getAccuracy(), Toast.LENGTH_SHORT).show();
     	System.out.println(mCurrentLocation.getLatitude());
     	try {
-			filewriter.write("Location is:" + mCurrentLocation.getLatitude()+" "+mCurrentLocation.getLongitude()+" and accuracy is "+mCurrentLocation.getAccuracy());
+			filewriter.write("Location is:" + mCurrentLocation.getLatitude()+" "+mCurrentLocation.getLongitude()+" and accuracy is "+mCurrentLocation.getAccuracy()+"\n");
     	} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -107,5 +189,26 @@ GooglePlayServicesClient.OnConnectionFailedListener {
         // Display the connection status
         Toast.makeText(this, "Disconnected. Please re-connect.",
                 Toast.LENGTH_SHORT).show();
+	}
+
+	@Override
+	public void onLocationChanged(Location location) {
+		// Report to the UI that the location was updated
+		if(location!=null){
+			String msg = "Updated Location: " +
+						Double.toString(location.getLatitude()) + "," +
+						Double.toString(location.getLongitude())+ " and accuracy is "+location.getAccuracy();
+			Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+			try {
+				filewriter.write("Updated Location: " +
+						Double.toString(location.getLatitude()) + "," +
+						Double.toString(location.getLongitude())+ " and accuracy is "+location.getAccuracy()+"\n");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		else {
+			Toast.makeText(this, "Trying", Toast.LENGTH_SHORT).show();
+		}
 	}
 }
